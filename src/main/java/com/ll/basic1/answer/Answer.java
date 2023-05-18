743a0caab57e129c03bace1457b5fd420e8c42a0
Jserin <jsr3643@naver.com>
2023-05-18 13:48:46 +0900

    SBB05181348

---
+++ src/main/java/com/ll/basic1/answer/Answer.java
@@ -0,0 +1,28 @@
+package com.ll.basic1.answer;
+
+import com.ll.basic1.question.Question;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
--- src/test/java/com/ll/basic1/Basic1ApplicationTests.java
+++ src/test/java/com/ll/basic1/Basic1ApplicationTests.java
@@ -1,13 +1,37 @@
 package com.ll.basic1;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
+import com.ll.basic1.answer.Answer;
+import com.ll.basic1.answer.AnswerRepository;
+import com.ll.basic1.question.Question;
+import com.ll.basic1.question.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 @SpringBootTest
-class Basic1ApplicationTests {
+class SbbApplicationTests {
+
+	@Autowired
+	private QuestionRepository questionRepository;
+	@Autowired
+	private AnswerRepository answerRepository;
 
 	@Test
-	void contextLoads() {
-	}
+	void testJpa() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
 
+		Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+		a.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(a);
+	}
 }
